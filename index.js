fdbbba76e0597fa89e5d42640b7bd7dc98f0161c
thuannguyen88 <getaholdofthuan@gmail.com>
2021-12-06 17:46:02 +0000

    task 4, installed chalk and changed colors of collection

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-// 👉 2c. Create a function called `describeItem`, which should take in an item as a parameter (the argument handed to this function would be an item from our collection). The function should `console.log` a message according to how many of the item you have.
+import myCollection from "./collection.js";
 
-// If there's only one of the item in your collection, it should log "I have a `name`. Here's what I like about it: `whatILike`". If you have more than one of it, the message should log "I have `count` `name`s. Here's what I like about them: `whatILike`".
-
-// For example, the result of calling the `describeItem` function with the first item in our collection would be:
-// _"I have a School of Code mug. Here's what I like about it: It has my cute pixel character on it!"_
-
-// Call your function below where you've defined it, handing in the first item in `myCollection`.
-
-// Now run the file again (using `node index.js`);
-
-import myCollection from "./collection.js"
+import chalk from "chalk";
 
 function describeItem(myCollection) {
-    if (myCollection.count === 1) {
-      console.log("I have a " + myCollection.name + ". Here's what I like about it: " + myCollection.whatILike);
-    } else {
-      console.log("I have " + myCollection.count + " " + myCollection.name + ". Here's what I like about it: " + myCollection.whatILike);
-      }
+  if (myCollection.count === 1) {
+    console.log(
+      "I have a " +
+        chalk.cyan(myCollection.name) +
+        ". Here's what I like about it: " +
+        chalk.green(myCollection.whatILike)
+    );
+  } else {
+    console.log(
+      "I have " +
+        chalk.yellow(myCollection.count) +
+        " " +
+        chalk.cyan(myCollection.name) +
+        ". Here's what I like about it: " +
+        chalk.green(myCollection.whatILike)
+    );
+  }
 }
 
+console.log(chalk.yellow("hello"));
 // describeItem(myCollection[0]);
 
 // console.log(myCollection[0].count);
