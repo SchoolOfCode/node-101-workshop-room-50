fa72fa86e4663b248dd704cd4ee5d856ecccf1bd
sandraskolarczyk <spts20@sussex.ac.uk>
2021-12-06 17:23:11 +0000

    task 2d done

--- index.js
+++ index.js
@@ -28,25 +28,25 @@ let myCollection = [
 ];
 
 function describeItem(myCollection) {
-  if (myCollection.count === 1) {
-    console.log(
-      "I have a " +
-        myCollection.name +
-        ". Here's what I like about it: " +
-        myCollection.whatILike
-    );
-  } else if (myCollection.count > 1) {
-    console.log(
-      "I have " +
-        myCollection.count +
-        " " +
-        myCollection.name +
-        ". Here's what I like about it: " +
-        myCollection.whatILike
-    );
-  }
+    if (myCollection.count === 1) {
+      console.log("I have a " + myCollection.name + ". Here's what I like about it: " + myCollection.whatILike);
+    } else {
+      console.log("I have " + myCollection.count + " " + myCollection.name + ". Here's what I like about it: " + myCollection.whatILike);
+      }
 }
 
-describeItem(myCollection[1]);
+// describeItem(myCollection[0]);
 
 // console.log(myCollection[0].count);
+
+// Now make a function called describeCollection that takes in an array as a parameter. The function should loop through the array, and for each item, it should call the describeItem function so it displays a message according to how many you have in your collection.
+
+// Call your function below where you've defined it, handing in the myCollection array.
+
+// Run the file again to see the output and check that it outputs the correct message for each item in your collection (i.e. each object in your myCollection array).
+
+function describeCollection(arr) {
+  arr.forEach(describeItem);
+}
+
+describeCollection(myCollection);
